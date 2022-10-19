af85bdf06e87a7bd92e8ccde98ca69dbb0c646c0
MagStrength <101830802+MagStrength@users.noreply.github.com>
2022-10-19 21:33:58 +0300

    Add files via upload

---
+++ public/js/webpack.js
@@ -0,0 +1,12 @@
+import CartComponent from './components/cart-component';
+import ErrorComponent from './components/error-component';
+import FilterComponent from './components/filter-component';
+import ProductsComponent from './components/products-component';
+
+export default {
+    components:
+        CartComponent,
+    ErrorComponent,
+    FilterComponent,
+    ProductsComponent,
+}
